ca14e8042b78a2286851806a0b59186b270a2530
eom-eom <elbertmolina_25@hotmail.com>
2016-03-18 17:23:10 +0800

    Overall

--- PayrollSytemV2/page/Department.aspx.vb
+++ PayrollSytemV2/page/Department.aspx.vb
@@ -8,7 +8,6 @@ Public Class Department
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         txtDeptDesc.Enabled = False
         txtDeptName.Enabled = False
-       
         fillDept()
     End Sub
 
