fa967b3d4ad07c193f6ce6aac10e0978f59565d8
jayem028 <jeremy.dequito@sparkglobaltech.com>
2016-04-05 10:55:04 +0800

    report

---
+++ PayrollSytemV2/page/Reports.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/index.Master" CodeBehind="Reports.aspx.vb" Inherits="PayrollSytemV2.Reports" %>
+
+<%@ Register Assembly="CrystalDecisions.Web, Version=13.0.2000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" Namespace="CrystalDecisions.Web" TagPrefix="CR" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="contentHeader" runat="server">
+    Reports
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="contentSmall" runat="server">
+    Control Panel
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="contentBC" runat="server">
+    Reports
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="contentBody" runat="server">
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            <div class="box box-warning">
+            <div class="box-body">
+            <div class="col-md-12">
+                 <div class="row">
+                    <div class="col-md-12">
+                        <div class="messagealert" id="alert_container">
+                        </div>
+                    </div>
+                </div>
+                  
+                 <div class="row">
+                     <br />
+                     <div class="row">
+                        <div class="col-md-2">
+                            <h5>Payroll Code</h5>
+                        </div>
+                        <div class="col-md-4">
+                           <asp:DropDownList class="form-control select2"  ID="ddlPayrollCode" runat="server" AutoPostBack="false" >
+                            </asp:DropDownList> 
+                        </div>
+                        <div class="col-md-2">
+                            <asp:Button ID="btnSearch" runat="server" Text="Search" class="btn btn-block btn-primary btn-md" />
+                        </div>
+                    </div>
+                    
+                 </div>
+                 <div class="row">
+                     <CR:CrystalReportViewer ID="crvReports" runat="server" AutoDataBind="true" Visible="true" />
+                 </div>
+             </div>
+             </div>
+             </div>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</asp:Content>
---
+++ PayrollSytemV2/page/Reports.aspx.vb
@@ -0,0 +1,92 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Reports
+    Inherits System.Web.UI.Page
+    Dim rptPayrollJournal As New payrolljournal
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+            FillPCodeDDL
+        End If
+    End Sub
+    Public Sub FillPCodeDDL()
+        Dim cdb As New CReports
+        Dim dt As DataTable = cdb.ShiftsListDDL
+
+        ddlPayrollCode.DataValueField = "payroll_code"
+        ddlPayrollCode.DataTextField = "v_payroll_code"
+
+        ddlPayrollCode.DataSource = dt
+        ddlPayrollCode.DataBind()
+    End Sub
+    Private Sub PayrollJournal(Optional ByVal xEmpCode As String = "")
+        Try
+            Using SQLConnect As New MySqlConnection(My.Settings.DBConn)
+                SQLConnect.Open()
+                Dim xSQL As New System.Text.StringBuilder
+                xSQL.AppendLine("SELECT payroll_details.id, ")
+                xSQL.AppendLine("    payroll_details.payroll_code, ")
+                xSQL.AppendLine("    payroll_details.emp_code, ")
+                xSQL.AppendLine("    payroll_details.emp_tax_comp, ")
+                xSQL.AppendLine("    payroll_details.emp_basicpay, ")
+                xSQL.AppendLine("    payroll_details.emp_late, ")
+                xSQL.AppendLine("    payroll_details.emp_absent, ")
+                xSQL.AppendLine("    payroll_details.emp_ut, ")
+                xSQL.AppendLine("    payroll_details.emp_ot, ")
+                xSQL.AppendLine("    payroll_details.emp_taxallowance, ")
+                xSQL.AppendLine("    payroll_details.emp_receivable, ")
+                xSQL.AppendLine("    payroll_details.emp_deminimis, ")
+                xSQL.AppendLine("    payroll_details.emp_totaltaxearning, ")
+                xSQL.AppendLine("    payroll_details.emp_bonus, ")
+                xSQL.AppendLine("    payroll_details.emp_wtax, ")
+                xSQL.AppendLine("    payroll_details.emp_govdeduc, ")
+                xSQL.AppendLine("    payroll_details.emp_comdeduc, ")
+                xSQL.AppendLine("    payroll_details.emp_totaldeduction, ")
+                xSQL.AppendLine("    payroll_details.emp_netpay, ")
+                xSQL.AppendLine("    payroll_details.emp_payrolladjustment, ")
+                xSQL.AppendLine("    payroll_details.emp_payroll_year, ")
+                xSQL.AppendLine("    payroll_details.is_deleted, ")
+                xSQL.AppendLine("    SSS.govde_eeshare as 'SSS', ")
+                xSQL.AppendLine("    PhilHealth.govde_eeshare as 'PhilHealth', ")
+                xSQL.AppendLine("    HDMF.govde_eeshare as 'HDMF' ")
+                xSQL.AppendLine("FROM payroll_details ")
+                xSQL.AppendLine("    LEFT OUTER JOIN payroll_govde as SSS ON payroll_details.payroll_code = SSS.payroll_code and payroll_details.emp_code = SSS.emp_code and SSS.gov_desc = 'SSS' ")
+                xSQL.AppendLine("    LEFT OUTER JOIN payroll_govde as PhilHealth ON payroll_details.payroll_code = PhilHealth.payroll_code and payroll_details.emp_code = PhilHealth.emp_code and PhilHealth.gov_desc = 'PhilHealth' ")
+                xSQL.AppendLine("    LEFT OUTER JOIN payroll_govde as HDMF ON payroll_details.payroll_code = HDMF.payroll_code and payroll_details.emp_code = HDMF.emp_code and HDMF.gov_desc = 'HDMF' ")
+                xSQL.AppendLine("WHERE payroll_details.payroll_code = '" & Trim(ddlPayrollCode.SelectedValue) & "' ")
+
+
+                'If Len(xEmpCode) > 0 Then
+                '    xSQL.AppendLine("AND PayrollDetail.emp_code = '" & Trim(xEmpCode) & "' ")
+                'xSQL.AppendLine("WHERE payroll_details.emp_code = 'EMP-0001' ")
+                'End If
+                Dim SQLCommand As New MySqlCommand(xSQL.ToString, SQLConnect)
+                Dim da As New MySqlDataAdapter(SQLCommand)
+                Dim ds As New DataSet
+                da.Fill(ds)
+                If ds.Tables.Count <> 0 Then
+                    ds.WriteXml(HttpRuntime.AppDomainAppPath & "\XML\PayrollJournal.xml", XmlWriteMode.WriteSchema)
+                End If
+
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Protected Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        PayrollJournal()
+
+        Dim dsPayrollJournal As New DataSet
+        dsPayrollJournal = New DSREPORT()
+        Dim dsPayrollJournalTemp As New DataSet
+        dsPayrollJournal = New DataSet()
+        dsPayrollJournal.Tables.Add("PayrollJournal")
+        rptPayrollJournal = New payrolljournal
+        rptPayrollJournal.SetDataSource(dsPayrollJournal.Tables(0))
+        dsPayrollJournalTemp.ReadXml(HttpRuntime.AppDomainAppPath & "\XML\PayrollJournal.xml")
+        dsPayrollJournal.Merge(dsPayrollJournalTemp.Tables(0))
+        crvReports.ReportSource = rptPayrollJournal
+
+    End Sub
+End Class
